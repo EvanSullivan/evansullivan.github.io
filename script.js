67d1411efb6571448cee93a6eb73a1a81fa03424
Evan Sullivan <darkfox3889@gmail.com>
2016-02-27 18:10:56 -0800

    Centering and work block start.
    Added new Sun Risk image.
    Centered headings.
    Increased text size a bit.
    Cleaned up various parameters.

---
+++ script.js
@@ -0,0 +1,36 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+    "use strict";
+    
+//**************************************
+//**************************************	
+//Auto-Scroll Links
+//**************************************
+//**************************************
+//fun scroll function
+//    function scrollToAnchor(anchor) {
+//        var aTag = $("a[id='" + anchor + "']");
+//        $('html,body').animate({scrollTop: aTag.offset().top - 60}, 800);
+//    }
+//
+//	function activateScroll(e) {
+//		if (e.target !== e.currentTarget) {
+//
+//			var clickedItem = e.target.id;
+//            clickedItem = clickedItem.replace('link', '');
+//			scrollToAnchor(clickedItem);
+//
+//		}
+//		e.stopPropagation();
+//	}
+//    
+//    //assign scroll to elements in menubar
+//	var menuBar = document.getElementsByClassName('headerBar')[0];
+//    console.log(menuBar);
+//	menuBar.addEventListener("click", activateScroll, false);
+    
+});
+
+console.log("JS still connected @ end");
